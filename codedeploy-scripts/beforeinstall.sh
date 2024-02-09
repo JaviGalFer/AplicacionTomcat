d27b793680b8dddbaee7d650d0b744a82334c3de
JaviGalFer <fgalfer299@g.educaand.es>
2024-02-09 18:52:57 +0100

    update

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,23 +1,23 @@
 #-->DESPLIEGUE
-# Clonar el repositorio de GitHub
-cd /tmp
-git clone https://github.com/JaviGalFer/AplicacionTomcat
-cd AplicacionTomcat
-# Apagamos Tomcat
-systemctl stop tomcat
+
 # Hacer ejecutable gradlew
-chmod +x gradlew
+chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/gradlew
+
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
 
 # Compilar la aplicación Java
 bash gradlew war
 
 # Mover el archivo WAR resultante a la carpeta de aplicaciones de Tomcat
-mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/AplicacionTomcat.war
+# mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/AplicacionTomcat.war
+
+mv AplicacionTomcat/build/libs/holamundo-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/AplicacionTomcat.war
+
 
 # Crear directorio para desplegar la aplicación (opcional)
-mkdir /opt/tomcat/webapps/AplicacionTomcat
-unzip /opt/tomcat/webapps/AplicacionTomcat.war -d /opt/tomcat/webapps/AplicacionTomcat
+#mkdir /opt/tomcat/webapps/AplicacionTomcat
+#unzip /opt/tomcat/webapps/AplicacionTomcat.war -d /opt/tomcat/webapps/AplicacionTomcat
 # Volvemos a iniciar Tomcat
-systemctl start tomcat
+#systemctl start tomcat
 # Añadir salida que devuelve la ruta a la aplicación instalada
 echo "La aplicación Java ha sido instalada en: /opt/tomcat/webapps/AplicacionTomcat"

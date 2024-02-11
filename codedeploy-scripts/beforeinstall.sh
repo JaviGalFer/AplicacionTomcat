20b77355a871d170b6862ec322be33290fb156b7
JaviGalFer <fgalfer299@g.educaand.es>
2024-02-11 15:51:16 +0100

    update

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -8,19 +8,5 @@ cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/dep
 # Compilar la aplicación Java
 bash gradlew war
 
-# Mover el archivo WAR resultante a la carpeta de aplicaciones de Tomcat
-# mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/AplicacionTomcat.war
-
-# mv AplicacionTomcat/build/libs/holamundo-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/AplicacionTomcat.war
-
-mv /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/build/libs/holamundo-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/AplicacionTomcat.war
-
-
-
-# Crear directorio para desplegar la aplicación (opcional)
-#mkdir /opt/tomcat/webapps/AplicacionTomcat
-#unzip /opt/tomcat/webapps/AplicacionTomcat.war -d /opt/tomcat/webapps/AplicacionTomcat
-# Volvemos a iniciar Tomcat
-#systemctl start tomcat
 # Añadir salida que devuelve la ruta a la aplicación instalada
 echo "La aplicación Java ha sido instalada en: /opt/tomcat/webapps/AplicacionTomcat"
